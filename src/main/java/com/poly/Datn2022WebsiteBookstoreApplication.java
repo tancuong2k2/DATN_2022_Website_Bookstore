1869ee9290fb27871f7189700cbf4233b1d02c97
nguyenvu98 <70495495+nguyenvu98@users.noreply.github.com>
2022-09-16 10:38:47 +0700

    Initial commit

---
+++ src/main/java/com/poly/Datn2022WebsiteBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datn2022WebsiteBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Datn2022WebsiteBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/Datn2022WebsiteBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Datn2022WebsiteBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
